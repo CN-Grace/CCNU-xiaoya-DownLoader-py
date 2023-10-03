2af1d99d26aee2ad3f371f62b90de7b7e0026caa
zhouxinghua001 <834513403@qq.com>
2023-10-03 16:02:23 +0800

    second commit

--- 小雅.py
+++ 小雅.py
@@ -1,5 +1,6 @@
 import os
 import re
+from typing import Any, TextIO
 
 import requests
 
@@ -12,9 +13,58 @@ def get_json(url: str, headers: dict) -> bool | dict:
         return False
 
 
+def get_course(headers: dict) -> tuple[list[Any], list[Any]]:
+    url = "https://ccnu.ai-augmented.com/api/jx-iresource/group/student/groups?time_flag=1"
+    data = get_json(url=url, headers=headers).get("data")
+    return [i.get("name") for i in data], [i.get("id") for i in data]
+
+
+def choose(name_list: list, id_list: list) -> None:
+    for i in range(len(name_list)):
+        print(f"{i}.{name_list[i]}")
+    temp = int(input("请输入你要爬取的课程编号"))
+    while temp not in range(len(name_list)):
+        print("输入有误")
+        temp = int(input("请输入你要爬取的课程编号"))
+    url = ("https://ccnu.ai-augmented.com/api/jx-iresource/resource/queryCourseResources?group_id=" + id_list[temp])
+    download_yes = input("将下载可下载的视频资源，是否下载(Y/n):")
+    if download_yes == "N" or download_yes == "n":
+        video = False
+    else:
+        video = True
+    log = make_root(course_id=id_list[temp])
+    html = get_json(url=url, headers=headers)
+    file_list = data2list(data=html)
+    tree = list2tree(file_list=file_list)
+    makedir_and_download(file_tree=tree, headers=headers, video=video, log=log)
+
+
 def data2list(data: dict) -> list:
     return [{"id": i.get("id"), "parent_id": i.get("parent_id"), "mimetype": i.get("mimetype"), "name": i.get("name"),
              "type": i.get("type"), "quote_id": i.get("quote_id"), } for i in data.get("data")]
+
+
+def make_root(course_id: str) -> TextIO:
+    name = (requests.post(url="https://ccnu.ai-augmented.com/api/jx-iresource/statistics/group/visit",
+                          data={"group_id": course_id, "role_type": "normal"}, headers=headers, ).json().get(
+        "data").get(
+        "name"))
+    log = open(f"download_filename-{name}.log", "w+", encoding="utf-8")
+    log.write("已创建" + os.getcwd() + "\\" + name + "\n")
+    log.flush()
+    i = 1
+    while os.path.exists(name):
+        if i == 1:
+            log.write(f"{name}-路径已存在")
+        try:
+            log.write(f"将其重名为{name}({i})")
+            os.rename(name, f"{name}({i})")
+        except:
+            log.write(f"{name}({i})已存在")
+            i = i + 1
+    os.mkdir(name)
+    os.chdir(name)
+    return log
 
 
 def list2tree(file_list: list) -> dict:
@@ -71,7 +121,7 @@ def download_video(item_json: dict, headers: dict, log) -> None:
         m3u8_url: str = (
             get_json(url=get_m3u8_url, headers=headers).get("data").get("private_vod")[0].get("private_url"))
         m3u8_list: str = requests.get(url=m3u8_url, headers=headers).text
-        ts_list: file_list = re.findall(pattern=".*?\.ts", string=m3u8_list)
+        ts_list: list = re.findall(pattern=".*?\.ts", string=m3u8_list)
         with open(name, "ab+") as f:
             for i in ts_list:
                 url: str = "https://vod-trans-1.ccnu.edu.cn" + i
@@ -79,42 +129,12 @@ def download_video(item_json: dict, headers: dict, log) -> None:
             log.write("已下载" + os.getcwd() + "\\" + name + "\n")
             log.flush()
     except:
-        log.write("暂时无法下载"+os.getcwd() + "\\" + name + "\n")
+        log.write("暂时无法下载" + os.getcwd() + "\\" + name + "\n")
         log.flush()
 
 
 if __name__ == "__main__":
     Authorization = input("请输入您的Authorization：")
-    temp = input("请输入需要爬取的课程地址：")
-    download_yes = input("将下载可下载的视频资源，是否下载(Y/n):")
-    if download_yes == "N" or download_yes == "n":
-        video = False
-    else:
-        video = True
-    print(download_yes,video)
-    pattern = re.compile("(?<=mycourse/)([0-9]*?)(?=/resource)")
-    temp = re.search(pattern=pattern, string=temp).group()
-    url = ("https://ccnu.ai-augmented.com/api/jx-iresource/resource/queryCourseResources?group_id=" + temp)
     headers = {"Authorization": Authorization}
-    name = (requests.post(url="https://ccnu.ai-augmented.com/api/jx-iresource/statistics/group/visit",
-                          data={"group_id": temp, "role_type": "normal"}, headers=headers, ).json().get("data").get(
-        "name"))
-    log = open(f"download_filename-{name}.log", "w+", encoding="utf-8")
-    log.write("已创建" + os.getcwd() + "\\" + name + "\n")
-    log.flush()
-    i=1
-    while os.path.exists(name):
-        if i ==1:
-            log.write(f"{name}-路径已存在")
-        try:
-            log.write(f"将其重名为{name}({i})")
-            os.rename(name, f"{name}({i})")
-        except:
-            log.write(f"{name}({i})已存在")
-            i=i+1
-    os.mkdir(name)
-    os.chdir(name)
-    html = get_json(url=url, headers=headers)
-    file_list = data2list(data=html)
-    tree = list2tree(file_list=file_list)
-    makedir_and_download(file_tree=tree, headers=headers, video=video, log=log)
+    name_list, id_list = get_course(headers=headers)
+    choose(name_list=name_list, id_list=id_list)
